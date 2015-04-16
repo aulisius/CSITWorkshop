f60511ed71c2e0afe652a8807d462703a7dc8878
faizaanceg <aulisius7@gmail.com>
2015-04-16 07:55:33 +0530

    added functionality, completed

--- app/src/main/java/com/example/faizaan/converter/MainActivity.java
+++ app/src/main/java/com/example/faizaan/converter/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.faizaan.gallerydemo;
+package com.example.faizaan.converter;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
-
 
 public class MainActivity extends Activity {
 
@@ -27,24 +26,30 @@ public class MainActivity extends Activity {
     }
 
     public void radioToggle(View v) {
-
-        if( v.getId() == R.id.radioButton)
+        if( v.getId() == R.id.radioButton ) {
+            cel.setChecked(true);
             fah.setChecked(false);
-        else
-            fah.setChecked(true);
-    }
-    public void calc(View v) {
-
-        Double n = Double.parseDouble( num.toString() ), t;
-
-        if(cel.isChecked()) {
-            t = (n - 32) * (5/9);
         }
         else {
-            t = (n * (9 / 5)) + 32;
+            fah.setChecked(true);
+            cel.setChecked(false);
         }
-
-        Toast.makeText( getApplicationContext(), t.toString(), Toast.LENGTH_LONG ).show();
     }
 
+    public void calc(View v) {
+        try {
+
+            float n =  Float.parseFloat(num.getText().toString()), t;
+            if(!Float.isNaN(n)) {
+                t = cel.isChecked() ? (float) ((n - 32) * (0.55555555555)) : (float) (n * 1.8) + 32;
+                Toast.makeText( getApplicationContext(), Float.toString(t), Toast.LENGTH_LONG ).show();
+            }
+            else Toast.makeText(getApplicationContext(), "Wrong input", Toast.LENGTH_LONG).show();
+
+        }
+        catch(NumberFormatException e){Toast.makeText(getApplicationContext(), "Wrong input", Toast.LENGTH_SHORT).show();}
+
+
+
     }
+}
